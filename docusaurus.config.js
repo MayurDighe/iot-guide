6bd0e9403341dea0ccc9853e9a0d922fead54c15
Mayur Dighe <themayurdighe@gmail.com>
2024-08-11 14:24:11 +0000

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,63 @@
+module.exports = {
+    title: 'Internet of Things (IoT)',
+    tagline: 'Exploring the Connected World',
+    url: 'https://iotguide.in',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
+    organizationName: 'MayurDighe', // Your GitHub org/user name.
+    projectName: 'iotguide', // Your repo name.
+    themeConfig: {
+      navbar: {
+        title: 'IoT',
+        logo: {
+          alt: 'IoT Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {to: 'docs/intro', label: 'Docs', position: 'left'},
+          {to: 'blog', label: 'Blog', position: 'left'},
+          {to: 'about', label: 'About', position: 'left'},
+          {href: 'https://github.com/MayurDighe/iotguide', label: 'GitHub', position: 'right'},
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {label: 'Introduction', to: '/docs/intro'},
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {label: 'GitHub', href: 'https://github.com/MayurDighe/iotguide'},
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Mayur Dighe. Built with Docusaurus.`,
+      },
+    },
+    presets: [
+      [
+        '@docusaurus/preset-classic',
+        {
+          docs: {
+            sidebarPath: require.resolve('./sidebars.js'),
+            editUrl: 'https://github.com/MayurDighe/iotguide/edit/main/website/',
+          },
+          blog: {
+            showReadingTime: true,
+            editUrl: 'https://github.com/MayurDighe/iotguide/edit/main/website/blog/',
+          },
+          theme: {
+            customCss: require.resolve('./src/css/custom.css'),
+          },
+        },
+      ],
+    ],
+  };
+  
---
+++ sidebars.js
@@ -0,0 +1,105 @@
+module.exports = {
+    docs: [
+      {
+        type: 'category',
+        label: 'Introduction',
+        items: ['intro'],
+      },
+      {
+        type: 'category',
+        label: 'IoT Architecture',
+        items: ['architecture'],
+      },
+      {
+        type: 'category',
+        label: 'IoT Devices',
+        items: ['devices'],
+      },
+      {
+        type: 'category',
+        label: 'Connectivity in IoT',
+        items: ['connectivity'],
+      },
+      {
+        type: 'category',
+        label: 'IoT Data Management',
+        items: ['data-management'],
+      },
+      {
+        type: 'category',
+        label: 'IoT Security',
+        items: ['security'],
+      },
+      {
+        type: 'category',
+        label: 'IoT Applications',
+        items: ['applications'],
+      },
+      {
+        type: 'category',
+        label: 'IoT in Smart Cities',
+        items: ['smart-cities'],
+      },
+      {
+        type: 'category',
+        label: 'IoT in Agriculture',
+        items: ['agriculture'],
+      },
+      {
+        type: 'category',
+        label: 'IoT in Retail',
+        items: ['retail'],
+      },
+      {
+        type: 'category',
+        label: 'IoT in Healthcare',
+        items: ['healthcare'],
+      },
+      {
+        type: 'category',
+        label: 'IoT in Manufacturing',
+        items: ['manufacturing'],
+      },
+      {
+        type: 'category',
+        label: 'IoT Standards and Regulations',
+        items: ['standards-regulations'],
+      },
+      {
+        type: 'category',
+        label: 'IoT Development Tools',
+        items: ['development-tools'],
+      },
+      {
+        type: 'category',
+        label: 'IoT Analytics',
+        items: ['analytics'],
+      },
+      {
+        type: 'category',
+        label: 'IoT and Artificial Intelligence',
+        items: ['ai'],
+      },
+      {
+        type: 'category',
+        label: 'IoT and Blockchain',
+        items: ['blockchain'],
+      },
+      {
+        type: 'category',
+        label: 'Future of IoT',
+        items: ['future'],
+      },
+      {
+        type: 'category',
+        label: 'IoT Case Studies',
+        items: ['case-studies'],
+      },
+      {
+        type: 'category',
+        label: 'IoT Resources',
+        items: ['resources'],
+      },
+    ],
+  };
+  
